fc1c156c03bfaa6278d8126e88f07c47afa04e12
UllaMontonen <u.montonen@gmail.com>
2022-10-11 12:57:50 +0300

    Adding tests to project

--- src/test/java/Projekti/Bookstore/RepositoryTests.java
+++ src/test/java/Projekti/Bookstore/RepositoryTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 import Projekti.Bookstore.domain.ApplicationUserRepository;
 import Projekti.Bookstore.domain.Book;
 import Projekti.Bookstore.domain.BookRepository;
@@ -22,18 +24,26 @@ public class RepositoryTests {
 	@Autowired
 	ApplicationUserRepository applicationUserRepository;
 	
-	// Testataan hakua
+	// Testataan hakua - ei toimi - laita opettajalle
 	@Test
 	public void findBook() {
-		Book book = bookRepository.findById((long) 2).get();
+		Book book = bookRepository.findById((long) 1).get();
 		System.out.println("Haetaan id:llä 2 " + book);
-		assertEquals(book.getId(), 2);
+		assertEquals(book.getAuthor(), "Elly Griffiths");
 	}
 	
+	// Testataan hakua - haetaan Elly Griffithsin kirjoja
+	@Test
+	public void findByCategory() {
+		//haetaan kyseisen kirjailijan kirjat - niitä on yksi
+		List<Book> books = bookRepository.findByAuthor("Elly Griffiths");
+		assertEquals(books.size(), 1);
+	}
 	
 	// Testataan kirjan luomista
 	@Test
 	public void insertNewBook() {
+		// luodaan uusi kirja
 		Book book = new Book("Kirjan Otsikko", "Nimi", 2020, "9789520418168", 7);
 		bookRepository.save(book);
 		assertNotNull(book.getId());
@@ -42,6 +52,7 @@ public class RepositoryTests {
 	// Testataan kirjan poistamista
 	@Test
 	public void deleteBook() {
+		bookRepository.deleteById((long) 1);
 		
 	}
 	
